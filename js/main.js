99c6c0f6f19612c75911b1de4dc63e2e00087291
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-11 14:43:00 +0100

    testing distance

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ function renderPlaces(places) {
 function printDist() {
   let distTest = document.getElementById("distanceTest");
 
-  console.log(`${distTest.getAttribute("id")}`);
+  console.log(`${distTest.getAttribute("distanceMsg")}`);
 }
 
 fetchContact();
