c139b570bed79ce2efdde723af59a788f75b18bf
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-11 13:57:06 +0100

    testing distance

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function renderPlaces(places) {
       let model = document.createElement("a-entity");
       let text = document.createElement("a-entity");
       let pinImage = document.createElement("a-image");
-      let dist = document.createElement("a-entity");
+      // let dist = document.createElement("a-entity");
 
       model.setAttribute(
         "gps-entity-place",
@@ -64,9 +64,9 @@ function renderPlaces(places) {
       //     "distanceMsg"
       //   )} "; align: center; color: green;`
       // );
-      dist.setAttribute("look-at", "#camra");
-      dist.setAttribute("scale", "8 8 8");
-      dist.setAttribute("position", "0 1.1 0");
+      // dist.setAttribute("look-at", "#camra");
+      // dist.setAttribute("scale", "8 8 8");
+      // dist.setAttribute("position", "0 1.1 0");
 
       text.setAttribute(
         "text",
