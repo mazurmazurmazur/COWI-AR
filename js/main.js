3c9f57741d11a02f3cbee2464fb09f230015c2f7
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-11 11:17:01 +0100

    testing distance

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function showPosition(position) {
     position.coords.latitude +
     "<br>Longitude: " +
     position.coords.longitude;
-  +"<br>" + document.getElementById("testDistance").getAttribute("src");
+  +"<br>" + document.getElementById("distanceTest");
 }
 
 function yourFunction() {
