0fb186fedd050f82f68ccaae271320b8af093b65
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-18 13:40:18 +0100

    testing connecting points with lines

--- js/main.js
+++ js/main.js
@@ -126,15 +126,24 @@ function connectPoints() {
       console.log(previousPoint.getAttribute("position"));
 
       point.setAttribute(
-        "meshline",
-        `lineWidth: 5px; path: ${previousPoint.getAttribute("position").x} ${
-          previousPoint.getAttribute("position").y
-        } ${previousPoint.getAttribute("position").z}, ${currentPosition.x} ${
-          currentPosition.y
-        } ${currentPosition.z};  color: red`
+        "line",
+        `start: ${previousPoint.getAttribute("position").x +
+          0.00001} ${previousPoint.getAttribute("position").y +
+          0.00001} ${previousPoint.getAttribute("position").z +
+          0.00001}; end: ${currentPosition.x} ${currentPosition.y} ${
+          currentPosition.z
+        };  color: red`
       );
     } else {
-      console.log("handle that");
+      point.setAttribute(
+        "line",
+        `start: ${currentPosition.x} ${currentPosition.y} ${currentPosition.z}; end: 10 0 10; color: red`
+      );
+
+      point.setAttribute(
+        "line__2",
+        `start: -10 0 15; end: ${currentPosition.x} ${currentPosition.y} ${currentPosition.z}; color: red`
+      );
     }
     console.log(`current point: `);
     console.log(point.getAttribute("position"));
