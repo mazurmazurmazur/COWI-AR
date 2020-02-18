9a4d8e857f0ee897d3a671a5a27f3a9751c0f069
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-18 11:33:27 +0100

    testing connecting points with lines

--- js/main.js
+++ js/main.js
@@ -124,7 +124,7 @@ function connectPoints() {
     if (previousPosition)
       point.setAttribute(
         "line",
-        `end: ${currentPosition.x} ${currentPosition.y} ${currentPosition.z}; start: 26.904014351285348 0 -47.76847894997331; color: red`
+        `start: ${currentPosition.x} ${currentPosition.y} ${currentPosition.z}; end: 26.904014351285348 0 -47.76847894997331; color: red`
       );
     else {
       point.setAttribute(
