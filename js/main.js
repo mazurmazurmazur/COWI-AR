ce64c6413c2a8e3ffee3ad01cf5bea2b93206ad3
Krzysztof Mazur <KRMAX@COWI.Com>
2020-03-17 08:23:43 +0100

    tests for report purposes

--- js/main.js
+++ js/main.js
@@ -112,12 +112,13 @@ function renderPlaces(places) {  //adding elements with coordinates to scene
         let longitude = coordinate[1];
         // let altitude = coordinate[2] ? coordinate[2] : -2; //if altitude set, use it, otherwise set it to -2
         let model = document.createElement("a-entity");
+        model.setAttribute("position", '0 -2 0');
+
         let pinImage = document.createElement("a-image");
         model.setAttribute(
           "gps-entity-place",
           `latitude: ${longitude}; longitude: ${latitude};`
         );
-        model.setAttribute("position", '0 -2 0');
         model.classList.add("geoPoint", "geo" + placeIndex);
         // pinImage.setAttribute("src", "./assets/marker.png");
         pinImage.setAttribute("look-at", "#camra");
